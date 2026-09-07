6d30ccee624743fbf13e2c4ffb132cbe4374d569
Luciano dos Santos <lucds2.ls@gmail.com>
2026-09-07 14:21:24 -0300

    fix: atualiza conexao postgresql para usar DATABASE_URL e SSL

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
-import  Sequelize  from "sequelize";
-import databaseconfig from '../config/database.cjs'
+import Sequelize from "sequelize";
+import databaseconfig from '../config/database.cjs';
 import User from "../app/models/User.js";
 import Product from "../app/models/Product.js";
 import Category from "../app/models/Category.js";
@@ -8,23 +8,42 @@ import mongoose from "mongoose";
 const models = [User, Product, Category];
 
 class Database {
-    constructor(){
-this.init();
-this.mongo();
+  constructor() {
+    this.init();
+    this.mongo();
+  }
 
+  init() {
+    // Se existir a variável DATABASE_URL (no Render), usa ela; caso contrário, usa o databaseconfig padrão
+    if (process.env.DATABASE_URL) {
+      this.connection = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        define: {
+          timestamps: true,
+          underscored: true,
+          underscoredAll: true,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // Necessário para aceitar conexões SSL no Render
+          },
+        },
+      });
+    } else {
+      this.connection = new Sequelize(databaseconfig);
     }
 
-    init(){
-        this.connection = new Sequelize(databaseconfig);
-        models.map((models)=> models.init(this.connection)).map((models)=> models.associate && models.associate(this.connection.models));
-    }
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
+  }
 
-    mongo() {
+  mongo() {
     this.mongooseConnection = mongoose.connect(
-        process.env.MONGO_URL || 'mongodb://localhost:27017/devburger'
+      process.env.MONGO_URL || 'mongodb://localhost:27017/devburger'
     );
+  }
 }
-}
-
 
 export default new Database();
