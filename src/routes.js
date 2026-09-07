5f8eb1b0f43a79d65fcc85d3cc592198b44259a8
Luciano dos Santos <lucds2.ls@gmail.com>
2026-09-07 16:29:13 -0300

    fix: corrige extensao e sintaxe do multer para ES modules

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import UserController from "./app/controllers/UserController.js";
 import SessionController from "./app/controllers/SessionController.js";
 import ProductsController from "./app/controllers/ProductsController.js";
 import multer from "multer";
-import multerConfig from "./config/multer.cjs";
+import multerConfig from "./config/multer.js";
 import authmiddlwares from "./app/middlewares/auth.js";
 import CategoryController from "./app/controllers/CategoryController.js";
 import adminMiddlwares from "./app/middlewares/admin.js";
