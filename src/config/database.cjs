b0f8473acc6b198d69a53dcaa9b2198f7fc40427
Luciano dos Santos <lucds2.ls@gmail.com>
2026-09-07 14:30:23 -0300

    fix: adiciona suporte a DATABASE_URL e SSL no database.cjs

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'devburger',
-    logging: false,
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-    },
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL,
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'devburger',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL
+      ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      : false,
+  },
 };
-
