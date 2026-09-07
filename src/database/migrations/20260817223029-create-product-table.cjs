ff7dd200e5b9824b6e3e2e27f6261e7ca54c9d9e
Luciano dos Santos <lucds2.ls@gmail.com>
2026-09-07 17:27:16 -0300

    fix: atualiza migration e seed de produtos

--- src/database/migrations/20260817223029-create-product-table.cjs
+++ src/database/migrations/20260817223029-create-product-table.cjs
@@ -3,24 +3,49 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-
-await queryInterface.createTable('products', { 
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-  name: { type: Sequelize.STRING, allowNull: true },
-  price: { type: Sequelize.INTEGER, allowNull: false },
-  path: { type: Sequelize.STRING, allowNull: false },
-  category: { type: Sequelize.STRING, allowNull: false },
-  created_at: { type: Sequelize.DATE, allowNull: false },
-  updated_at: { type: Sequelize.DATE, allowNull: false }
- });
-     
+    await queryInterface.createTable('products', { 
+      id: { 
+        type: Sequelize.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true, 
+        allowNull: false 
+      },
+      name: { 
+        type: Sequelize.STRING, 
+        allowNull: false 
+      },
+      price: { 
+        type: Sequelize.INTEGER, 
+        allowNull: false 
+      },
+      path: { 
+        type: Sequelize.STRING, 
+        allowNull: false 
+      },
+      offer: { 
+        type: Sequelize.BOOLEAN, 
+        defaultValue: false, 
+        allowNull: false 
+      },
+      category_id: { 
+        type: Sequelize.INTEGER, 
+        allowNull: false,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      created_at: { 
+        type: Sequelize.DATE, 
+        allowNull: false 
+      },
+      updated_at: { 
+        type: Sequelize.DATE, 
+        allowNull: false 
+      }
+    });
   },
 
   async down (queryInterface) {
-
     await queryInterface.dropTable('products');
-    
   }
 };
-
-
