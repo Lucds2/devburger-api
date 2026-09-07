97a3ecdcb58645f9c84935a7ef3875f9a5bd36e4
Luciano dos Santos <lucds2.ls@gmail.com>
2026-09-07 18:47:58 -0300

    retirado rota delete via get

--- src/routes.js
+++ src/routes.js
@@ -15,15 +15,15 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 
-// Rota temporária para limpar a tabela de produtos
-routes.get('/products/delete-all', async (req, res) => {
-  try {
-    await Product.destroy({ where: {}, truncate: true, cascade: true });
-    return res.status(200).json({ message: 'Todos os produtos foram apagados com sucesso!' });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-});
+// // Rota temporária para limpar a tabela de produtos
+// routes.get('/products/delete-all', async (req, res) => {
+//   try {
+//     await Product.destroy({ where: {}, truncate: true, cascade: true });
+//     return res.status(200).json({ message: 'Todos os produtos foram apagados com sucesso!' });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
 
 // Rotas Públicas
 routes.post('/user', UserController.store);
